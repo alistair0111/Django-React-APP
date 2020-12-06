63d752a4fc609418528d3e225352902483b90cd7
alistair0111 <alistair.baretto@gmail,com>
2020-12-06 09:19:36 +0530

    Leave Room Managed

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -17,6 +17,7 @@ export default class HomePage extends Component {
     this.state = {
       roomCode: null
     }
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -51,18 +52,31 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null
+    })
+  }
+
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" render={() => {
-            return this.state.roomCode ? (
-            <Redirect to={`/room/${this.state.roomCode}`}/>
-            ) : (this.renderHomePage());
-          }}/>
+          <Route exact path="/" 
+            render={() => {
+              return this.state.roomCode ? (
+              <Redirect to={`/room/${this.state.roomCode}`}/>
+              ) : (this.renderHomePage());
+            }}
+          />
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path='/room/:roomCode' component={Room} />
+          <Route 
+            path='/room/:roomCode' 
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback = {this.clearRoomCode}/>
+            }}
+          />
         </Switch>
       </Router>
     );
